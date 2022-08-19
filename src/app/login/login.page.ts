2b97a35fde4a9a679f6da0486b2788fe897152e1
sofitismi <111587300+sofitismi@users.noreply.github.com>
2022-08-19 12:49:04 -0300

    subida inicial del proyecto

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
