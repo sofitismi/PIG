75f7095f70650db68ee621f3e097276e729fa3ee
MetaLuciana <111587326+MetaLuciana@users.noreply.github.com>
2022-09-29 10:57:48 -0300

    arreglos Lu

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
-import { UserPhoto, PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -9,31 +8,10 @@ import { UserPhoto, PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+  constructor(public actionSheetController: ActionSheetController) {}
 
   async ngOnInit() {
-    await this.photoService.loadSaved();
   }
 
-  public async showActionSheet(photo: UserPhoto, position: number) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Photos',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          this.photoService.deletePicture(photo, position);
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          // Nothing to do, action sheet is automatically closed
-         }
-      }]
-    });
-    await actionSheet.present();
-  }
+  
 }
