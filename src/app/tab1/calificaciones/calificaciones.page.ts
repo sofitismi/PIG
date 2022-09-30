55a1767bd60ffdfc733f2a1f8befd05d18b091f5
sofitismi <111587300+sofitismi@users.noreply.github.com>
2022-09-30 11:26:39 -0300

    cambios sofi

--- src/app/tab1/calificaciones/calificaciones.page.ts
+++ src/app/tab1/calificaciones/calificaciones.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-calificaciones',
   templateUrl: './calificaciones.page.html',
   styleUrls: ['./calificaciones.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CalificacionesPage implements OnInit {
 
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TabsPage {
 
