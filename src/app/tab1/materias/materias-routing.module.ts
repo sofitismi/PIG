b9e51d3ace651738b523313edef182595d6d249e
Ju4n4s0l <112090546+Ju4n4s0l@users.noreply.github.com>
2022-09-30 10:55:28 -0300

    cambios jua

--- src/app/tab1/materias/materias-routing.module.ts
+++ src/app/tab1/materias/materias-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: MateriasPage
+  },  {
+    path: 'detalle',
+    loadChildren: () => import('./detalle/detalle.module').then( m => m.DetallePageModule)
   }
+
 ];
 
 @NgModule({
