748de9ab8d8abf094da7a82e58f11e1c97cb3fe1
MetaLuciana <111587326+MetaLuciana@users.noreply.github.com>
2022-10-18 09:37:27 -0300

    new lu

--- src/app/tab1/materias/detalle/detalle.page.ts
+++ src/app/tab1/materias/detalle/detalle.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.page.html',
   styleUrls: ['./detalle.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DetallePage implements OnInit {
 
